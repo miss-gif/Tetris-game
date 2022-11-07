1c86ce1c7f85f709673103931d9c7d6af8a3d042
miss <ddc.akari@gmail.com>
2022-11-08 00:36:34 +0900

    test

---
+++ JS/test.js
@@ -0,0 +1,3 @@
+for (let i = 0; i < 20; i++) {
+  const element = 20;
+}
--- JS/tetris.js
+++ JS/tetris.js
@@ -1,6 +1,8 @@
 // DOM
 const playground = document.querySelector(".playground > ul");
 
+console.log(playground);
+
 // Setting
 const GAME_ROWS = 20;
 const GAME_COLS = 10;
@@ -14,7 +16,7 @@ let tempMovingItem;
 const BLOCKS = {
   tree: [
     [
-      [0, 0],
+      [2, 1],
       [0, 1],
       [1, 0],
       [1, 1],
@@ -54,5 +56,53 @@ function prependNewLine() {
 
 function renderBlocks() {
   const { type, direction, top, left } = tempMovingItem;
-  BLOCKS[type][direction];
+  const movingBlocks = document.querySelectorAll(".moving");
+  movingBlocks.forEach((moving) => {
+    moving.classList.remove(type, "moving");
+  });
+  BLOCKS[type][direction].forEach((block) => {
+    const x = block[0] + left;
+    const y = block[1] + top;
+    const target = playground.childNodes[y] ? playground.childNodes[y].childNodes[0].childNodes[x] : null;
+    const isAvailable = checkEmpty(target);
+    if (isAvailable) {
+      target.classList.add(type, "moving");
+    } else {
+      tempMovingItem = { ...movingItem };
+      setTimeout(() => {
+        renderBlocks();
+      }, 0);
+    }
+  });
 }
+
+function checkEmpty(target) {
+  if (!target) {
+    return false;
+  }
+  return true;
+}
+
+function moveBlock(moveType, amount) {
+  tempMovingItem[moveType] += amount;
+  renderBlocks();
+}
+
+// event handling
+document.addEventListener("keydown", (e) => {
+  switch (e.keyCode) {
+    case 39:
+      moveBlock("left", 1);
+      break;
+    case 37:
+      moveBlock("left", -1);
+      break;
+    case 40:
+      moveBlock("top", 1);
+      break;
+
+    default:
+      break;
+  }
+  console.log(e);
+});
